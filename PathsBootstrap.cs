dcef4cf76ce81d572dd0d751027ba2c73e0396b7
jarosm3@gmail.com <jarosm3@gmail.com>
2025-04-25 12:01:32 +0200

    Add script to download Bootstrap assets and define paths for usage

---
+++ PathsBootstrap.cs
@@ -0,0 +1,59 @@
+namespace Cameek.W3CssJs.Bootstrap;
+
+public static class PathsBootstrap
+{
+    /// <summary>
+    /// Bootstrap Main CSS
+    /// </summary>
+    public static string BootstrapMinCss => "_content/Cameek.W3CssJs.Bootstrap/css/bootstrap/bootstrap.min.css";
+
+    /// <summary>
+    /// Bootstrap RTL Main CSS
+    /// </summary>
+    public static string BootstrapRtlMinCss => "_content/Cameek.W3CssJs.Bootstrap/css/bootstrap/bootstrap.rtl.min.css";
+
+    /// <summary>
+    /// Bootstrap Grid CSS
+    /// </summary>
+    public static string BootstrapGridMinCss => "_content/Cameek.W3CssJs.Bootstrap/css/bootstrap/bootstrap-grid.min.css";
+
+    /// <summary>
+    /// Bootstrap Grid RTL CSS
+    /// </summary>
+    public static string BootstrapGridRtlMinCss => "_content/Cameek.W3CssJs.Bootstrap/css/bootstrap/bootstrap-grid.rtl.min.css";
+
+    /// <summary>
+    /// Bootstrap Reboot CSS
+    /// </summary>
+    public static string BootstrapRebootMinCss => "_content/Cameek.W3CssJs.Bootstrap/css/bootstrap/bootstrap-reboot.min.css";
+
+    /// <summary>
+    /// Bootstrap Reboot RTL CSS
+    /// </summary>
+    public static string BootstrapRebootRtlMinCss => "_content/Cameek.W3CssJs.Bootstrap/css/bootstrap/bootstrap-reboot.rtl.min.css";
+
+    /// <summary>
+    /// Bootstrap Utilities CSS
+    /// </summary>
+    public static string BootstrapUtilitiesMinCss => "_content/Cameek.W3CssJs.Bootstrap/css/bootstrap/bootstrap-utilities.min.css";
+
+    /// <summary>
+    /// Bootstrap Utilities RTL CSS
+    /// </summary>
+    public static string BootstrapUtilitiesRtlMinCss => "_content/Cameek.W3CssJs.Bootstrap/css/bootstrap/bootstrap-utilities.rtl.min.css";
+
+    /// <summary>
+    /// Bootstrap Bundle JS
+    /// </summary>
+    public static string BootstrapBundleMinJs => "_content/Cameek.W3CssJs.Bootstrap/js/bootstrap/bootstrap.bundle.min.js";
+
+    /// <summary>
+    /// Bootstrap ESM JS
+    /// </summary>
+    public static string BootstrapEsmMinJs => "_content/Cameek.W3CssJs.Bootstrap/js/bootstrap/bootstrap.esm.min.js";
+
+    /// <summary>
+    /// Bootstrap Main JS
+    /// </summary>
+    public static string BootstrapMinJs => "_content/Cameek.W3CssJs.Bootstrap/js/bootstrap/bootstrap.min.js";
+}
