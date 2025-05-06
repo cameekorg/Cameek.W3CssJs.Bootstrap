75b77f791b3a005bb70969cdd07a19e591447566
jarosm3@gmail.com <jarosm3@gmail.com>
2025-05-06 22:52:03 +0200

    Update Bootstrap version to 5.3.3 and enhance documentation for asset paths

--- PathsBootstrap.cs
+++ PathsBootstrap.cs
@@ -7,72 +7,86 @@ namespace Cameek.W3CssJs.Bootstrap;
 public static class PathsBootstrap
 {
     /// <summary>
-    /// Bootstrap Main CSS: _content/Cameek.W3CssJs.Bootstrap/css/bootstrap.min.css
+    /// Bootstrap Main CSS: 
+    /// <code>_content/Cameek.W3CssJs.Bootstrap/css/bootstrap.min.css</code>
     /// </summary>
     public static string BootstrapMinCss => "_content/Cameek.W3CssJs.Bootstrap/css/bootstrap.min.css";
 
     /// <summary>
-    /// Bootstrap RTL Main CSS: _content/Cameek.W3CssJs.Bootstrap/css/bootstrap.rtl.min.css
+    /// Bootstrap RTL Main CSS: 
+    /// <code>_content/Cameek.W3CssJs.Bootstrap/css/bootstrap.rtl.min.css</code>
     /// </summary>
     public static string BootstrapRtlMinCss => "_content/Cameek.W3CssJs.Bootstrap/css/bootstrap.rtl.min.css";
 
     /// <summary>
-    /// Bootstrap Grid CSS: _content/Cameek.W3CssJs.Bootstrap/css/bootstrap-grid.min.css
+    /// Bootstrap Grid CSS: 
+    /// <code>_content/Cameek.W3CssJs.Bootstrap/css/bootstrap-grid.min.css</code>
     /// </summary>
     public static string BootstrapGridMinCss => "_content/Cameek.W3CssJs.Bootstrap/css/bootstrap-grid.min.css";
 
     /// <summary>
-    /// Bootstrap Grid RTL CSS: _content/Cameek.W3CssJs.Bootstrap/css/bootstrap-grid.rtl.min.css
+    /// Bootstrap Grid RTL CSS: 
+    /// <code>_content/Cameek.W3CssJs.Bootstrap/css/bootstrap-grid.rtl.min.css</code>
     /// </summary>
     public static string BootstrapGridRtlMinCss => "_content/Cameek.W3CssJs.Bootstrap/css/bootstrap-grid.rtl.min.css";
 
     /// <summary>
-    /// Bootstrap Reboot CSS: _content/Cameek.W3CssJs.Bootstrap/css/bootstrap-reboot.min.css
+    /// Bootstrap Reboot CSS: 
+    /// <code>_content/Cameek.W3CssJs.Bootstrap/css/bootstrap-reboot.min.css</code>
     /// </summary>
     public static string BootstrapRebootMinCss => "_content/Cameek.W3CssJs.Bootstrap/css/bootstrap-reboot.min.css";
 
     /// <summary>
-    /// Bootstrap Reboot RTL CSS: _content/Cameek.W3CssJs.Bootstrap/css/bootstrap-reboot.rtl.min.css
+    /// Bootstrap Reboot RTL CSS: 
+    /// <code>_content/Cameek.W3CssJs.Bootstrap/css/bootstrap-reboot.rtl.min.css</code>
     /// </summary>
     public static string BootstrapRebootRtlMinCss => "_content/Cameek.W3CssJs.Bootstrap/css/bootstrap-reboot.rtl.min.css";
 
     /// <summary>
-    /// Bootstrap Utilities CSS: _content/Cameek.W3CssJs.Bootstrap/css/bootstrap-utilities.min.css
+    /// Bootstrap Utilities CSS: 
+    /// <code>_content/Cameek.W3CssJs.Bootstrap/css/bootstrap-utilities.min.css</code>
     /// </summary>
     public static string BootstrapUtilitiesMinCss => "_content/Cameek.W3CssJs.Bootstrap/css/bootstrap-utilities.min.css";
 
     /// <summary>
-    /// Bootstrap Utilities RTL CSS: _content/Cameek.W3CssJs.Bootstrap/css/bootstrap-utilities.rtl.min.css
+    /// Bootstrap Utilities RTL CSS: 
+    /// <code>_content/Cameek.W3CssJs.Bootstrap/css/bootstrap-utilities.rtl.min.css</code>
     /// </summary>
     public static string BootstrapUtilitiesRtlMinCss => "_content/Cameek.W3CssJs.Bootstrap/css/bootstrap-utilities.rtl.min.css";
 
     /// <summary>
-    /// Bootstrap Bundle JS (includes Popper): _content/Cameek.W3CssJs.Bootstrap/js/bootstrap.bundle.min.js
+    /// Bootstrap Bundle JS (includes Popper): 
+    /// <code>_content/Cameek.W3CssJs.Bootstrap/js/bootstrap.bundle.min.js</code>
     /// </summary>
     public static string BootstrapBundleMinJs => "_content/Cameek.W3CssJs.Bootstrap/js/bootstrap.bundle.min.js";
 
     /// <summary>
-    /// Bootstrap ESM JS (for ES module environments): _content/Cameek.W3CssJs.Bootstrap/js/bootstrap.esm.min.js
+    /// Bootstrap ESM JS (for ES module environments): 
+    /// <code>_content/Cameek.W3CssJs.Bootstrap/js/bootstrap.esm.min.js</code>
     /// </summary>
     public static string BootstrapEsmMinJs => "_content/Cameek.W3CssJs.Bootstrap/js/bootstrap.esm.min.js";
 
     /// <summary>
-    /// Bootstrap Main JS (no Popper): _content/Cameek.W3CssJs.Bootstrap/js/bootstrap.min.js
+    /// Bootstrap Main JS (no Popper): 
+    /// <code>_content/Cameek.W3CssJs.Bootstrap/js/bootstrap.min.js</code>
     /// </summary>
     public static string BootstrapMinJs => "_content/Cameek.W3CssJs.Bootstrap/js/bootstrap.min.js";
 
     /// <summary>
-    /// Bootstrap Icons font (WOFF): _content/Cameek.W3CssJs.Bootstrap/css/fonts/bootstrap-icons.woff
+    /// Bootstrap Icons font (WOFF): 
+    /// <code>_content/Cameek.W3CssJs.Bootstrap/css/fonts/bootstrap-icons.woff</code>
     /// </summary>
     public static string BootstrapIconsWoff => "_content/Cameek.W3CssJs.Bootstrap/css/fonts/bootstrap-icons.woff";
 
     /// <summary>
-    /// Bootstrap Icons font (WOFF2): _content/Cameek.W3CssJs.Bootstrap/css/fonts/bootstrap-icons.woff2
+    /// Bootstrap Icons font (WOFF2): 
+    /// <code>_content/Cameek.W3CssJs.Bootstrap/css/fonts/bootstrap-icons.woff2</code>
     /// </summary>
     public static string BootstrapIconsWoff2 => "_content/Cameek.W3CssJs.Bootstrap/css/fonts/bootstrap-icons.woff2";
 
     /// <summary>
-    /// Bootstrap Icons CSS: _content/Cameek.W3CssJs.Bootstrap/css/bootstrap-icons.css
+    /// Bootstrap Icons CSS: 
+    /// <code>_content/Cameek.W3CssJs.Bootstrap/css/bootstrap-icons.css</code>
     /// </summary>
     public static string BootstrapIconsCss => "_content/Cameek.W3CssJs.Bootstrap/css/bootstrap-icons.css";
 }
